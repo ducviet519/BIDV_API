bc7e70464a9cdc0792ece383da19db728f9aca18
DucViet519 <ducviet519@gmail.com>
2023-01-05 07:34:24 +0700

    ok

--- BIDV/Controllers/VBNBController.cs
+++ BIDV/Controllers/VBNBController.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BIDV.Controllers
@@ -19,18 +20,30 @@ namespace BIDV.Controllers
             _services = services;
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            string result = String.Empty;
+            List<Root> List = new List<Root>();
+            for (int page = 1; page <= 559; page++)
+            {
+                string json = await _services.VBNB.Get_Documents(page.ToString(), "vietld", "0975318195");
+                result = await _services.VBNB.Upsert_Documents(json);
+                Thread.Sleep(180000);
+            }
+
+            if (result == "OK") { return Ok(); }
+            else { return BadRequest(); }
+        }
+
         [HttpGet]
         public async Task<IActionResult> Get(int page)
         {
             string result = String.Empty;
             List<Root> List = new List<Root>();
-            //for (int page = 1; page <= 559; page++)
-            //{
-            //    string json = await _services.VBNB.Get_Documents(page.ToString(), "vietld", "0975318195");
-            //    result = await _services.VBNB.Upsert_Documents(json);
-            //}
+
             string json = await _services.VBNB.Get_Documents(page.ToString(), "vietld", "0975318195");
-                result = await _services.VBNB.Upsert_Documents(json);
+            result = await _services.VBNB.Upsert_Documents(json);
 
             if (result == "OK") { return Ok(); }
             else { return BadRequest(); }
